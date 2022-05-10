f22a5c601d912311f822e62fbc049f6f6312ec0f
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-10 12:06:21 +0200

    Premier script react

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
     </View>
   );
 }
